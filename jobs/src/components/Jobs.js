148e123f89a6f20ff5708b09d9a87925c641a3b6
Taha Chaoui <taha-chaoui@hotmail.com>
2020-10-13 12:13:17 +0100

    finished

--- jobs/src/components/Jobs.js
+++ jobs/src/components/Jobs.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Job from './Job'
 
 const Jobs = (props) => {
+
+    const[filtre, setFiltre] = useState('')
+    const[jobs, setJobs] = useState(props.jobs);
+
+    const filtrer = (e) => {
+        setFiltre(e.target.value);
+        const jobFiltred = e.target.value
+       ? jobs.filter((elem) => {
+            return elem.role
+            .toLowerCase()
+            .startsWith(e.target.value.toLowerCase());
+        })
+        : props.jobs
+        setJobs(jobFiltred);
+    };
+
     return (
         <main>
-            {props.jobs.map(job => (
+            <form>
+                <input onChange={(e) => filtrer(e)} value= {filtre}/>
+            </form>
+            {jobs.map(job => (
                 <Job key={job.id} job={job} />
             ))} 
         </main>
